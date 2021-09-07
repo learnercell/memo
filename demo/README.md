58bc8e2ce2f9d5ad484e1a90a6bbb39053b4ec7e
pengchao.zhao@xenera.jp <Tuq90946>
2021-09-07 15:51:07 +0800

    commit

---
+++ demo/README.md
@@ -0,0 +1,7 @@
+##spring boot demo
+
+---
+
+* __local swagger url__  [document](http://localhost:8080/swagger-ui.html)
+
++ **swagger docs** [docs](https://swagger.io/docs/specification/about/)
--- demo/src/main/kotlin/com/example/demo/controller/HtmlController.kt
+++ demo/src/main/kotlin/com/example/demo/controller/HtmlController.kt
@@ -1,15 +1,29 @@
 package com.example.demo.controller
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 
+@Tag(name = "test interface")
 @Controller
 class HtmlController {
 
     @GetMapping("/")
-    fun blog(model: Model): String {
+    @Operation(
+        summary = "request demo",
+        description = "spring boot request demo"
+    )
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "")
+        ]
+    )
+    fun demo(model: Model): String {
         model["title"] = "Demo"
         return "demo"
     }
